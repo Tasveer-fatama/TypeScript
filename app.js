849eb325613fa55707d55455b4ddbb4a5e8b3048
tasveer-fatama <tasveerfatema135@gmail.com>
2024-10-26 23:28:33 +0530

    added new file

---
+++ app.js
@@ -0,0 +1,11 @@
+var num = 20;
+console.log(num);
+var str = "apple";
+console.log(str);
+var isfalse = false;
+console.log(isfalse);
+function add(num1, num2) {
+    return num1 + num2;
+}
+var result = add(10, 20);
+console.log(result);
---
+++ app.ts
@@ -0,0 +1,16 @@
+const num:number = 20;
+console.log(num);
+
+const str:string = "apple";
+console.log(str);
+
+const isfalse:boolean =false;
+console.log(isfalse);
+
+
+function add(num1:number,num2:number){
+    return num1+num2;
+}
+ let result =add(10,20)
+ console.log(result);
+ 
