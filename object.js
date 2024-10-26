c14b23c6dc6fbbe0cb25f2b69540182c08642204
tasveer-fatama <tasveerfatema135@gmail.com>
2024-10-27 02:40:55 +0530

    Object.ts

---
+++ object.js
@@ -0,0 +1,6 @@
+var person = {
+    name: "tasveer",
+    age: 12,
+    IsFalse: true,
+};
+console.log(person.name);
---
+++ object.ts
@@ -0,0 +1,7 @@
+const person :{name:string,age:number,IsFalse:boolean}={
+name:"tasveer",
+age:12,
+IsFalse:true,
+}
+
+console.log(person.name);
